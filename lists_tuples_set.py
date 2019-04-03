e29e520d8b7526274ee9a09708b9658486f0e309
jvictorr <jvictorr@indracompany.com>
2019-04-03 13:24:49 -0300

    first commit

---
+++ lists_tuples_set.py
@@ -0,0 +1,14 @@
+my_variable = "hello"
+
+
+grades = [90, 78, 67, 90, 86]
+grades_tuples = (90, 78, 67, 90, 86, 90, 89) #imutable
+set_grades = {77, 8, 90, 100, 100, 90, 87, 90, 1900} # unique & unordered
+
+
+# print(sum(grades) / len(grades))
+# print(sum(grades_tuples) / len(grades_tuples))
+grades.append(108)
+
+
+print(set_grades)
---
+++ variables_methods.py
@@ -0,0 +1,41 @@
+a = 5
+b = 10
+my_variable = 56
+any_variable_nano = 10
+
+
+string_variable = "hello"
+single_e_quotes = 'strings can have single quotes'
+
+
+# print(my_variable)
+# print(string_variable)
+
+
+##
+
+def my_print_method(my_parameter):
+    print(my_parameter)
+
+my_print_method(string_variable)
+
+
+def met_util(number_one, number_two):
+    return number_one * number_two
+
+result = met_util(5, 3)
+# my_print_method(result)
+# my_print_method(met_util(5,9))
+
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_method(one, two):
+    return one * two
+
+result_set = my_method(12, 9)
+
+print(result_set)
+
+
+
+
